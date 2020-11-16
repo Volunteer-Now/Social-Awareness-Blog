966a62d73607c8c4b9bfd39fbfaeab35d3c59dca
Volunteer-Now <74514248+Volunteer-Now@users.noreply.github.com>
2020-11-15 20:00:52 -0500

    Update heroHeader.js

--- src/components/heroHeader.js
+++ src/components/heroHeader.js
@@ -21,7 +21,7 @@ export default () => (
           className="primary-content" 
           dangerouslySetInnerHTML={{ __html: data.site.siteMetadata.home.description}}
         />
-        <Link to='/contact' className="button -primary">Read latest article &rarr;</Link>
+        <Link to='https://von-blog.netlify.app/remark-kitchen-sink' className="button -primary">Read latest article &rarr;</Link>
       </div>
     )}
   />
