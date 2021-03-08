368c18da9928798f28fa6a03eb822a3dc0fd954c
Volunteer-Now <74514248+Volunteer-Now@users.noreply.github.com>
2021-03-08 16:37:01 -0500

    Update heroHeader.js

--- src/components/heroHeader.js
+++ src/components/heroHeader.js
@@ -21,7 +21,7 @@ export default () => (
           className="primary-content" 
           dangerouslySetInnerHTML={{ __html: data.site.siteMetadata.home.description}}
         />
-        <Link to='/redefining-volunteerism' className="button -primary">Read latest article &rarr;</Link>
+        <Link to='/cyber-seniors' className="button -primary">Read latest article &rarr;</Link>
       </div>
     )}
   />
